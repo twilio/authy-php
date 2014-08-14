d770adb9f8f44be87f87aba4ce7b9b0da79534ca
@tufla <henry@oppten.com>
2014-08-14 15:38:48 -0500

    Supporting previous version initialization. Fixing paths to locate new class.

--- Authy.php
+++ Authy.php
@@ -1,6 +1,10 @@
 <?php
-
-require __DIR__.'/vendor/autoload.php';
+$filename = '/vendor/autoload.php';
+$paths = [__DIR__.$filename, __DIR__.'/../../..'.$filename];
+foreach($paths as $path){
+    if (file_exists($path))
+        require $path;
+}
 
 class Authy_Api extends Authy\AuthyApi {}
 
