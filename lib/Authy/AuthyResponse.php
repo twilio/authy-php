a00f694f05b08240513061f62b3cf018dc4369b1
Vic D'Elfant <vicdelfant@midd.ag>
2015-11-08 16:11:22 +0100

    Applied necessary changes for Guzzle 6

--- lib/Authy/AuthyResponse.php
+++ lib/Authy/AuthyResponse.php
@@ -37,7 +37,7 @@ class AuthyResponse
     public function __construct($raw_response)
     {
         $this->raw_response = $raw_response;
-        $this->body = (! isset($raw_response->body)) ? $raw_response->json(['object' => true]) : $raw_response->body;;
+        $this->body = (! isset($raw_response->body)) ? json_decode($raw_response->getBody()) : $raw_response->body;;
         $this->errors = new \stdClass();
 
         // Handle errors
